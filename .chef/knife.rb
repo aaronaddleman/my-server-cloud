dc6dbe57caae166934766b7e0fd385076914b1ee
Aaron Addleman <aaronaddleman@gmail.com>
2015-05-30 15:07:32 -0700

    playground with chef server and linux servers

---
+++ .chef/knife.rb
@@ -0,0 +1,30 @@
+# log_level                 :info
+# log_location              STDOUT
+# node_name                 'leo'
+# client_key                "./aaron.pem"
+# syntax_check_cache_path   "#{current_dir}/syntax_check_cache"
+# validation_client_name    "chef-validator"
+# validation_key            "#{current_dir}/chef-validator.pem"
+# chef_server_url           "https://10.33.33.33" 
+# cookbook_path             ["#{current_dir}/../cookbooks", "#{current_dir}/../site-cookbooks"] 
+# node_path                 "#{current_dir}/../nodes" 
+# role_path                 "#{current_dir}/../roles"
+# data_bag_path             "#{current_dir}/../data_bags"
+# environment_path          "#{current_dir}/../environments"
+# # encrypted_data_bag_secret "data_bag_key" 
+
+# knife[:berkshelf_path] = "cookbooks"
+
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "aaddleman"
+client_key               "#{current_dir}/aaddleman.pem"
+validation_client_name   "aaroninc-validator"
+validation_key           "#{current_dir}/aaroninc-validator.pem"
+chef_server_url          "https://10.33.33.33/organizations/aaroninc"
+syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntaxcache"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+ssl_verify_mode          :verify_none
---
+++ Berksfile
@@ -0,0 +1,7 @@
+source "https://supermarket.chef.io"
+
+#cookbook 'chef-server', '~> 2.1.6'
+cookbook 'chef-server'
+cookbook 'nginx'
+cookbook 'yum'
+cookbook 'sysctl', '~> 0.6.2'
